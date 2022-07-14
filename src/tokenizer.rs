712e6885ea4c8449a889db139811f53e33be6d99
Felix Marezki <fmarezki@gmail.com>
2022-07-14 13:20:48 +0200

    wip

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -40,13 +40,17 @@ impl<'a> Cursor<'a> {
     fn new(subject: &'a str) -> Self {
         Self {
             start: 0,
-            end: 0,
+            end: 1,
             subject,
         }
     }
 
-    fn slice(&self) -> &'a str {
-        &self.subject[self.start..self.end]
+    fn slice(&self) -> Option<&'a str> {
+        if self.end < self.subject.len() {
+            Some(&self.subject[self.start..self.end])
+        } else {
+            None
+        }
     }
 
     fn inc_end(&mut self) {
@@ -59,6 +63,7 @@ impl<'a> Cursor<'a> {
 
     fn next(&mut self) {
         self.start = self.end;
+        self.end += 1;
     }
 
     fn peek(&self) -> Option<&str> {
@@ -91,7 +96,7 @@ impl<'a, T> From<(&Cursor<'a>, T)> for Token<'a, T> {
                 start: tuple.0.start,
                 end: tuple.0.end,
             },
-            content: tuple.0.slice(),
+            content: tuple.0.slice().unwrap(),
             token_type: tuple.1,
         }
     }
@@ -105,14 +110,17 @@ struct TokenizerError<'a> {
 
 #[derive(Debug, PartialEq)]
 enum TokenType {
-    Number,      // Introspect
-    String,      // 4u32, "String",
+    Number,
+    String,
     StructStart, // (
     StructEnd,   // )
     ArrayStart,  // [
     ArrayEnd,    // ]
     DictStart,   // {
     DictEnd,     // }
+    DictAssignmentOperator,
+    Seperator,
+    Whitespace,
 }
 
 struct Tokenizer {
@@ -124,40 +132,41 @@ struct Tokenizer {
     array_end_regex: Regex,
     dict_start_regex: Regex,
     dict_end_regex: Regex,
-    seperator: Regex,
+    dict_assignment: Regex,
+    seperator_regex: Regex,
+    whitespace_regex: Regex,
 }
 
 impl Tokenizer {
     fn new() -> Self {
         Self {
-            number_regex: Regex::new(r"^-?[0-9]+(\.[0-9]+)?$").unwrap(),
-            string_regex: Regex::new(r"^'.+'$").unwrap(),
+            number_regex: Regex::new(r"^?-[0-9]+?(\.[0-9]+)$").unwrap(),
+            string_regex: Regex::new(r#"^(".*")|('.*')$"#).unwrap(),
             struct_start_regex: Regex::new(r"^\($").unwrap(),
             struct_end_regex: Regex::new(r"^\)$").unwrap(),
             array_start_regex: Regex::new(r"^\]$").unwrap(),
             array_end_regex: Regex::new(r"^\[$").unwrap(),
             dict_start_regex: Regex::new(r"^\{$").unwrap(),
             dict_end_regex: Regex::new(r"^\}$").unwrap(),
-            seperator: Regex::new(r"^,[ ]*$").unwrap(),
+            seperator_regex: Regex::new(r"^,$").unwrap(),
+            whitespace_regex: Regex::new(r"^[ ]+$").unwrap(),
+            dict_assignment: Regex::new(r"^:$").unwrap(),
         }
     }
 
     fn tokenize<'a>(&self, sub: &'a str) -> Result<TokenStream<'a, TokenType>, TokenizerError> {
+        // TDOD rework
+
         let mut cursor = Cursor::new(sub);
         let mut token_stream = TokenStream::default();
 
-        while let Some(slice) = cursor.peek() {
+        while let Some(slice) = cursor.slice() {
             match self.match_token(slice) {
                 Some(token) => {
-                    cursor.inc_end();
-                    if cursor.empty() {
-                        token_stream.push((&cursor, token).into())
-                    }
-                }
-                None => {
-                    token_stream.push((&cursor, self.match_token(cursor.slice()).unwrap()).into());
+                    token_stream.push((&cursor, token).into());
                     cursor.next();
                 }
+                None => cursor.inc_end(),
             }
         }
 
@@ -173,6 +182,8 @@ impl Tokenizer {
             Some(TokenType::DictStart)
         } else if self.dict_end_regex.is_match(slice) {
             Some(TokenType::DictEnd)
+        } else if self.dict_assignment.is_match(slice) {
+            Some(TokenType::DictAssignmentOperator)
         } else if self.array_start_regex.is_match(slice) {
             Some(TokenType::ArrayStart)
         } else if self.array_end_regex.is_match(slice) {
@@ -181,6 +192,10 @@ impl Tokenizer {
             Some(TokenType::String)
         } else if self.number_regex.is_match(slice) {
             Some(TokenType::Number)
+        } else if self.seperator_regex.is_match(slice) {
+            Some(TokenType::Seperator)
+        } else if self.whitespace_regex.is_match(slice) {
+            Some(TokenType::Whitespace)
         } else {
             None
         }
@@ -208,6 +223,129 @@ mod test {
                     content: ")",
                     token_type: TokenType::StructEnd
                 }
+            ]),
+            token_stream.unwrap()
+        );
+    }
+
+    #[test]
+    fn test_complex_structure() {
+        let tokenizer = Tokenizer::new();
+        let token_stream =
+            tokenizer.tokenize("(\"test\", -3.1, { \"key\": \"value\" }, [1, 2, 3])");
+
+        assert_eq!(
+            TokenStream::new(vec![
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: "(",
+                    token_type: TokenType::StructStart
+                },
+                Token {
+                    span: super::Span { start: 1, end: 7 },
+                    content: "\"test\"",
+                    token_type: TokenType::String
+                },
+                Token {
+                    span: super::Span { start: 8, end: 9 },
+                    content: ",",
+                    token_type: TokenType::Seperator
+                },
+                Token {
+                    span: super::Span { start: 9, end: 10 },
+                    content: " ",
+                    token_type: TokenType::Whitespace
+                },
+                Token {
+                    span: super::Span { start: 10, end: 13 },
+                    content: "-3.1",
+                    token_type: TokenType::Number
+                },
+                Token {
+                    span: super::Span { start: 13, end: 14 },
+                    content: ",",
+                    token_type: TokenType::Seperator
+                },
+                Token {
+                    span: super::Span { start: 14, end: 15 },
+                    content: " ",
+                    token_type: TokenType::Whitespace
+                },
+                Token {
+                    span: super::Span { start: 15, end: 16 },
+                    content: "{",
+                    token_type: TokenType::DictStart
+                },
+                Token {
+                    span: super::Span { start: 16, end: 17 },
+                    content: " ",
+                    token_type: TokenType::Whitespace
+                },
+                Token {
+                    span: super::Span { start: 17, end: 21 },
+                    content: "\"key\"",
+                    token_type: TokenType::String
+                },
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: ", ",
+                    token_type: TokenType::Seperator
+                },
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: "\"value\"",
+                    token_type: TokenType::String
+                },
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: "}",
+                    token_type: TokenType::DictEnd
+                },
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: ", ",
+                    token_type: TokenType::Seperator
+                },
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: "[",
+                    token_type: TokenType::ArrayStart
+                },
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: "1",
+                    token_type: TokenType::String
+                },
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: ", ",
+                    token_type: TokenType::Seperator
+                },
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: "2",
+                    token_type: TokenType::Number
+                },
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: ", ",
+                    token_type: TokenType::Seperator
+                },
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: "3",
+                    token_type: TokenType::Number
+                },
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: "]",
+                    token_type: TokenType::ArrayEnd
+                },
+                Token {
+                    span: super::Span { start: 0, end: 1 },
+                    content: ")",
+                    token_type: TokenType::StructEnd
+                },
             ]),
             token_stream.unwrap()
         );
